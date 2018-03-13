80c837dc9d9d584be93cfcc343f6cde2e9c06ab6
Chris <chris@uber5.com>
2018-03-13 17:52:00 +0200

    Configure eslint

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 // @flow
 /* eslint "import/prefer-default-export": "warn" */
 export { default as doGreatStuff } from './doGreatStuff'
+
+export { default as ActionButton } from './components/action-button'
