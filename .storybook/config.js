9650ea516de2f9e24a34b59bf6092de1dcb4d47e
Chris <chris@uber5.com>
2018-03-20 16:31:27 +0200

    Storybook config

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,12 @@
 import { configure } from '@storybook/react';
+import '@storybook/addon-console';
+import { setOptions } from '@storybook/addon-options';
+
+setOptions({
+  name: '@U5 - Components',
+  goFullScreen: true,
+  addonPanelInRight: true
+})
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /.stories.js$/);
